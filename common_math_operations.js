ac0d0326b02f0841e45bf8ec1fcfc32ae99cb648
Nina Tran <ninatran.az@gmail.com>
2022-05-18 17:20:08 -0500

    Add common math operations file

---
+++ common_math_operations.js
@@ -0,0 +1,32 @@
+/*
+    This JavaScript file contains functions that perform basic math operations
+*/
+
+/*
+    Adds two numbers
+*/
+function add(x, y){
+    return x + y;
+}
+
+
+/*
+    Multiplies number by 10
+*/
+function multiplyBy10(x){
+    return -1;
+}
+
+/*
+    Find the square root of a number
+*/
+function sqrt(x){
+    return -1
+}
+
+/*
+    Determines if a number is even
+*/
+function isEven(num){
+    return false
+}
