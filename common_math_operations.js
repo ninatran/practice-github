39ea101d2f7a43b6bcc140dc342263fa2375d9c9
Jerod Brackin <jbrackin1@gmail.com>
2022-05-19 12:44:10 -0500

    To watch starwars on telnet go type telnet towel.blinkenlights.nl on the terminal

--- common_math_operations.js
+++ common_math_operations.js
@@ -14,19 +14,17 @@ function add(x, y){
     Multiplies number by 10
 */
 function multiplyBy10(x){
-    return -1;
+    return x * 10;
 }
-
-/*
     Find the square root of a number
 */
 function sqrt(x){
-    return -1
+    return Math.sqrt(x);
 }
 
 /*
     Determines if a number is even
 */
 function isEven(num){
-    return false
+    if (num % 2 === 0) return true;
 }
